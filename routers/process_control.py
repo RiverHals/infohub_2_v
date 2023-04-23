c0f28ce1412167b53a3e4de1a382889489d12cb9
m.malyshkina <mar_mal05@mail.ru>
2023-04-23 22:19:09 +0300

    handles /api/pn/start and /api/pn/stop

--- routers/process_control.py
+++ routers/process_control.py
@@ -1,30 +1,49 @@
 from fastapi import APIRouter
+import sys
+import os
 import subprocess
-from ..model import Process
+import psutil
+sys.path.append(".")
+from model import Process
 
 router = APIRouter()
 
-@router.post("api/pn/start")
-def start_process(process: Process):
-    command = process.name
-    if command.is_process_running():
+def get_pid(process: Process):
+    try:
+        pid = subprocess.check_output(["pgrep", process.name])
+        return int(pid)
+    except:
+        return None
+def is_process_running(process: Process):
+    for proc in psutil.process_iter(['pid', 'name', 'status']):
+        if proc.info['name'] == "python":
+            return True
+    return False
+
+
+@router.post("/api/pn/start")
+async def start_process(process: Process):
+    # command = process.name
+    if is_process_running(process):
         return {"status": "already running"}
 
     try:
-        subprocess.Popen(["systemctl", "start", command])
-        return {"status": "started"}
+        with open("output.txt", "w") as f:
+            subprocess.Popen(process.name, shell=True, stdout=f, stderr=f, stdin=subprocess.PIPE)
+            return {"status": "started"}
     except:
         return {"status": "failed to start"}
 
 
-@router.post("api/pn/stop")
-def stop_process(process: Process):
-    command = process.name
-    if not command.is_process_running():
+@router.post("/api/pn/stop")
+async def stop_process(process: Process):
+    pid = get_pid(process)
+    if pid is None:
         return {"status": "not running"}
 
     try:
-        subprocess.Popen(["systemctl", "stop", command])
+        process = psutil.Process(pid)
+        process.terminate()
         return {"status": "stopped"}
     except:
         return {"status": "failed to stop"}
