362f881908c0a3fab203fb2fdde0e3fb06f0f090
m.malyshkina <mar_mal05@mail.ru>
2023-04-23 22:19:32 +0300

    handles /api/pn/result endpoint

--- routers/process_result.py
+++ routers/process_result.py
@@ -1,18 +1,34 @@
 from fastapi import APIRouter
+import sys
+import subprocess
 import psutil
-from ..model import Process
-from ..lib import get_pid
+sys.path.append(".")
+from model import Process
+from routers.lib import get_pid
+
+
+def get_pid(process: Process):
+    try:
+        pid = subprocess.check_output(["pgrep", process.name])
+        return int(pid)
+    except:
+        return None
+def is_process_running(process: Process):
+    for proc in psutil.process_iter(['pid', 'name', 'status']):
+        if proc.info['name'] == "python":
+            return True
+    return False
 
 router = APIRouter()
-@router.get("api/pn/result")
-def show_process_output(process: Process):
+@router.get("/api/pn/result")
+async def show_process_output(process: Process):
     pid = get_pid(process)
     if pid is None:
         return {"status": "process not found"}
 
     try:
-        process = psutil.Process(pid)
-        output, _ = process.communicate()
-        return {"output": output.decode("utf-8")}
+        with open("output.txt", "r") as f:
+            output = f.read()
+        return {"output": output}
     except:
         return {"status": "failed to get output"}
