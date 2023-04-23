228add06af014aa9fc5613754e96c70a4780e5de
m.malyshkina <mar_mal05@mail.ru>
2023-04-23 22:19:52 +0300

    handles the /api/pn endpoint

--- routers/process_status.py
+++ routers/process_status.py
@@ -1,11 +1,27 @@
 from fastapi import APIRouter
-from ..model import Process
+import subprocess
+import psutil
+import sys
+sys.path.append(".")
+from model import Process
+
+def get_pid(process: Process):
+    try:
+        pid = subprocess.check_output(["pgrep", process.name])
+        return int(pid)
+    except:
+        return None
+def is_process_running(process: Process):
+    for proc in psutil.process_iter(['pid', 'name', 'status']):
+        if proc.info['name'] == "python":
+            return True
+    return False
 
 router = APIRouter()
-@router.get("api/pn/")
-def status_process(process: Process):
+@router.get("/api/pn/")
+async def status_process(process: Process):
     command = process.name
-    if command.is_process_running():
+    if is_process_running(command):
         return {"status": "running"}
     else:
         return {"status": "not started"}
